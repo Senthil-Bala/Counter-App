1db08e1ce5e3e68f037113bcf52acda4d1e5b309
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-21 15:48:12 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   const [count, setcount] = useState(0)
   return <div className="App">
     <h1>Counter App</h1>
-    <h3>Count:{count}</h3>
-    <button onClick={()=>{setcount(count+1)}}>Increment</button>
-    <button onClick={()=>{setcount(count-1)}}>Decrement</button>
+    <h3>Count: {count}</h3>
+    <button onClick={()=>{setcount(prev=>prev+1)}}>Increment</button>
+    <button onClick={()=>{setcount(prev=>prev-1)}}>Decrement</button>
   </div>;
 }
 
