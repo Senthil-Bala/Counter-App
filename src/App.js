ee63437a99cafaa5dcd7efc6e0f523ec61d216f8
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-22 21:00:44 +0530

    class created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React , {useState} from 'react'
-import "./App.css";
+import React from 'react';
+import CounterApp from './CounterApp';
 
 function App() {
-  const [count, setcount] = useState(0)
-  return <div className="App">
-    <h1>Counter App</h1>
-    <h3>Count: {count}</h3>
-    <button onClick={()=>{setcount(prev=>prev+1)}}>Increment</button>
-    <button onClick={()=>{setcount(prev=>prev-1)}}>Decrement</button>
-  </div>;
+  return (
+    <div>
+      <CounterApp count={0} />
+    </div>
+  );
 }
 
 export default App;
+
